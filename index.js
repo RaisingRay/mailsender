419728f80011d29ce068a296bcad298a9a1427a0
MahmoudFredj <mahmoudrayfredj@gmail.com>
2023-02-07 01:53:31 +0300

    vercel deployment

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express();
 const mail = require('./mailSender.js');
 const PORT = process.env.PORT || 3000;
 
+app.get('/', async (req, res) => {
+    res.send('listening');
+})
 app.use("/api/mail", mail)
 app.listen(PORT, () => {
-    console.log('lisstening');
+    console.log('listening ...');
 });
