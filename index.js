63fb9924fd198773c0a50fab9b09a921028939ad
MahmoudFredj <mahmoudrayfredj@gmail.com>
2023-02-07 02:06:16 +0300

    give access to cors

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 const mail = require('./mailSender.js');
 const PORT = process.env.PORT || 3000;
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 app.get('/', async (req, res) => {
     res.send('listening');
 })
