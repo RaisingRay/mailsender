5ca2df907b83bcd303b03759b6fbfed7c6ca9cd5
MahmoudFredj <mahmoudrayfredj@gmail.com>
2023-02-06 23:49:09 +0300

    add main sender func

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('he')
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(() => {
+
+});
---
+++ mailSender.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/api/sendMail', async (req, res) => {
+    const msg = {
+        from: "atitbotnext@gmail.com",
+        to: "mf@atit-sa.com",
+        subject: "Recruit Bot from deployed app ⚡, " + req.body.status + " Profile",
+        html: parseResponse(req),
+    }
+
+    nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: "atitbotnext@gmail.com",
+            pass: "mhiniocwgzphvyjq",
+        },
+        port: 465,
+        host: 'smtp.gmail.com',
+    }).sendMail(msg, (err) => {
+        if (err) { res.status(500).send(err); }
+        else res.status(200).json(req.body);
+    })
+
+});
+
+module.exports = router;
