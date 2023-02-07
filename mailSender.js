7d299b1292c2c93e8a5eb8af11e1fd9e963e5bc6
MahmoudFredj <mahmoudrayfredj@gmail.com>
2023-02-07 10:18:54 +0300

    fix bug blocker

--- mailSender.js
+++ mailSender.js
@@ -3,6 +3,15 @@ const nodemailer = require('nodemailer');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
     const msg = {
         from: "atitbotnext@gmail.com",
         to: "mf@atit-sa.com",
